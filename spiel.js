dad0c742ba3fb998fbb42a65a21853e93163cb15
sakir tufan <sakirtufan49@gmail.com>
2021-02-28 12:04:17 +0100

    Touch Dedection, Click Events

--- spiel.js
+++ spiel.js
@@ -1,3 +1,19 @@
+// mobile check
+let isMobile = false;
+(function (a) {
+  if (
+    /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+      a
+    ) ||
+    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+      a.substr(0, 4)
+    )
+  )
+    isMobile = true;
+})(navigator.userAgent || navigator.vendor || window.opera);
+
+console.log(isMobile);
+
 // setup
 const c = document.createElement("canvas");
 c.width = window.innerWidth;
@@ -26,11 +42,11 @@ randomArray = shuffle(randomArray);
 randomArray.push(randomArray[0]); // Synchronize the first index with the last index to avoid conflict
 
 // Go from value a to value b in t steps
-const lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t * Math.PI))/2;
+const lerp = (a, b, t) => a + ((b - a) * (1 - Math.cos(t * Math.PI))) / 2;
 
 // draw more lines with fewer numbers
 const noise = (x) => {
-  x = x * 0.01 % 254;
+  x = (x * 0.01) % 254;
   return lerp(
     randomArray[Math.floor(x)],
     randomArray[Math.ceil(x)],
@@ -45,23 +61,26 @@ const lineColor = "#2f2519";
 const lineWidth = 5;
 const offset = -10;
 let step = 0;
-const yRatio = .2;
+const yRatio = 0.4;
 let speed = 0;
 let playing = true;
 
-const player = new function() {
+const player = new (function () {
   this.x = c.width / 2;
   this.y = 50;
   this.truck = new Image();
-  this.truck.src = "truck.png";
+  this.truck.src = "./UI/truck.png";
   this.rotation = 0;
   this.gravity = 0;
   this.rotationSpeed = 0;
 
-  this.draw = function () {
+  // interface
+  this.startButton = new Image();
+  this.startButton.src = "./UI/start.png";
 
-    let point1 = (c.height * .9) - noise(this.x + step) * yRatio;
-    let point2 = (c.height * .9) - noise(this.x + step + 5) * yRatio;
+  this.draw = function () {
+    let point1 = c.height * 0.9 - noise(this.x + step) * yRatio;
+    let point2 = c.height * 0.9 - noise(this.x + step + 5) * yRatio;
 
     let offset = 28;
 
@@ -75,27 +94,34 @@ const player = new function() {
       ground = 1;
     }
 
-    // Is the truck overturned?
-    if (!playing || ground && Math.abs(this.rotation) > Math.PI * .5) {
+    // fall check
+    if (!playing || (ground && Math.abs(this.rotation) > Math.PI * 0.5)) {
       playing = false;
       this.rotationSpeed = 5;
       this.x -= speed * 5;
 
-      context.textAlign = 'center';
-      context.textBaseline = 'middle';
+      context.textAlign = "center";
+      context.textBaseline = "middle";
       context.font = "32px Impact";
-      context.fillStyle = "white"
-      context.fillText("Game Over !", c.width / 2, c.height / 3)
+      context.fillStyle = "white";
+      context.fillText("Game Over !", c.width / 2, c.height / 3);
+      context.drawImage(
+        this.startButton,
+        c.width / 2 - 54,
+        c.height / 3 + 50,
+        108,
+        48
+      );
     }
-    
 
-    let angle = Math.atan2((point2 - offset) - this.y, (this.x + 5) - this.x);
+    // rotation calculator
+    let angle = Math.atan2(point2 - offset - this.y, this.x + 5 - this.x);
     if (ground && playing) {
       this.rotation -= (this.rotation - angle) * 0.5;
-      this.rotationSpeed = this.rotationSpeed - (angle - this.rotation)
+      this.rotationSpeed = this.rotationSpeed - (angle - this.rotation);
     }
 
-    this.rotation -= (this.rotationSpeed * 0.1)    
+    this.rotation -= this.rotationSpeed * 0.1;
 
     // fix the rotation problem
     if (this.rotation > Math.PI) this.rotation = -Math.PI;
@@ -107,21 +133,20 @@ const player = new function() {
     context.rotate(this.rotation);
     context.drawImage(this.truck, -40, -40, 80, 80);
     context.restore();
-  }
-}
+  };
+})();
 
 // draw
 function draw() {
-
   speed -= (speed - 1) * 0.01;
   step += 5 * speed;
-  
+
   // background
   context.fillStyle = bgColor;
   context.fillRect(0, 0, c.width, c.height);
 
   // player
-  player.draw()
+  player.draw();
 
   // ground
   context.fillStyle = foregroundColor;
@@ -129,18 +154,47 @@ function draw() {
   context.lineWidth = lineWidth;
   context.beginPath();
   context.moveTo(offset, c.height - offset);
-  
+
   for (let i = offset; i < c.width - offset; ++i) {
-    context.lineTo(i,(c.height * .9) - noise(i + step) * yRatio);
+    context.lineTo(i, c.height * 0.9 - noise(i + step) * yRatio);
   }
 
   context.lineTo(c.width - offset, c.height - offset);
   context.closePath();
   context.fill();
-  context.stroke();  
+  context.stroke();
 
   // animation
   requestAnimationFrame(draw);
 }
 
 draw();
+
+c.addEventListener("touchstart", handleStart, false);
+c.addEventListener("touchend", handleEnd, false);
+
+function handleStart(e) {
+  e.preventDefault();
+  let touches = e.changedTouches;
+  for (let i = 0; i < touches.length; i++) {
+    const touch = touches[i];    
+
+    if (
+      touch.pageX > c.width / 2 - 54 &&
+      touch.pageX < c.width / 2 + 54 &&
+      touch.pageY > c.height / 3 + 50 &&
+      touch.pageY < c.height / 3 + 98
+    ) {
+      window.location.reload();
+    }
+  }
+}
+
+function handleEnd(e) {
+  e.preventDefault();
+  let touches = e.changedTouches;
+  for (let i = 0; i < touches.length; i++) {
+    const touch = touches[i];
+    //console.log(touch.pageX, touch.pageY);
+  }
+}
